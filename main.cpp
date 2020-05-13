04d24434625b74625668b91a3634e6118c0d0ca3
Moriarty <shanguohang@zhidaoauto.com>
2020-05-13 11:02:08 +0800

    another serious of test

--- main.cpp
+++ main.cpp
@@ -10,20 +10,20 @@
 #include <Eigen/Core>
 #include <opencv2/opencv.hpp>
 
-#define XP 7
+#define XP 2
 #define YP 1
-#define ZP 7
+#define ZP 1
 
 class Zerror {
 private:
   std::vector<std::array<double, 3>> points;
 public:
-  static ceres::CostFunction* Create(std::vector<std::array<double, 3>> &_points) {
+  static ceres::CostFunction* Create(const std::vector<std::array<double, 3>> &_points) {
     return (new ceres::AutoDiffCostFunction<Zerror, 1, 3>(
       new Zerror(_points)));
   }
 
-  Zerror(std::vector<std::array<double, 3>> &_points) {
+  Zerror(const std::vector<std::array<double, 3>> &_points) {
     for (int i = 0; i < _points.size(); i++) {
       points.emplace_back(_points[i]);
     }
@@ -57,12 +57,12 @@ class Yerror {
 private:
   std::vector<std::array<double, 3>> points;
 public:
-  static ceres::CostFunction* Create(std::vector<std::array<double, 3>> &_points) {
+  static ceres::CostFunction* Create(const std::vector<std::array<double, 3>> &_points) {
     return (new ceres::AutoDiffCostFunction<Yerror, 1, 3>(
       new Yerror(_points)));
   }
 
-  Yerror(std::vector<std::array<double, 3>> &_points) {
+  Yerror(const std::vector<std::array<double, 3>> &_points) {
     for (int i = 0; i < _points.size(); i++) {
       points.emplace_back(_points[i]);
     }
@@ -96,12 +96,12 @@ class Xerror {
 private:
   std::vector<std::array<double, 3>> points;
 public:
-  static ceres::CostFunction* Create(std::vector<std::array<double, 3>> &_points) {
+  static ceres::CostFunction* Create(const std::vector<std::array<double, 3>> &_points) {
     return (new ceres::AutoDiffCostFunction<Xerror, 1, 3>(
       new Xerror(_points)));
   }
 
-  Xerror(std::vector<std::array<double, 3>> &_points) {
+  Xerror(const std::vector<std::array<double, 3>> &_points) {
     for (int i = 0; i < _points.size(); i++) {
       points.emplace_back(_points[i]);
     }
@@ -131,6 +131,31 @@ public:
 };
 
 
+void add_problems(
+    ceres::Problem& problem, double *r_vec,
+    const std::vector<cv::Point3d>& points,
+    std::vector<std::vector<int>>* places, int id) {
+  std::vector<std::array<double, 3>> use;
+  for (int i = 0; i < places[id].size(); i++) {
+    use.resize(places[id][i].size());
+    for (int j = 0; j < places[id][i].size(); j++) {
+      use[j][0] = points[places[id][i][j]].x;
+      use[j][1] = points[places[id][i][j]].y;
+      use[j][2] = points[places[id][i][j]].z;
+    }
+    ceres::CostFunction* costfunc;
+    if (id == 0) {
+      costfunc = Xerror::Create(use);
+    } else if (1 == id) {
+      costfunc = Yerror::Create(use);
+    } else if (2 == id) {
+      costfunc = Zerror::Create(use);
+    } else {
+      LOG(FATAL) << "wrong input id = " << id;
+    }
+    problem.AddResidualBlock(costfunc, nullptr, r_vec);
+  }
+}
 
 
 int main() {
@@ -138,65 +163,28 @@ int main() {
 
   ceres::Problem problem;
   double r_vec[3] = {1, 0, 0};
-#if 1
-  ypoints.emplace_back(std::array<double, 3>({ 1.56694, 1.23576, 8.09722 }));
-  ypoints.emplace_back(std::array<double, 3>({ 1.61549, 1.17857, 4.253 }));
-  ypoints.emplace_back(std::array<double, 3>({ 1.83658, 1.16982, 4.30788 }));
-  ypoints.emplace_back(std::array<double, 3>({ 1.63993, 1.12446, 7.39286 }));
-  ypoints.emplace_back(std::array<double, 3>({ -1.6854, 1.51227, 11.4332 }));
-  ypoints.emplace_back(std::array<double, 3>({ -1.47764, 1.41993, 5.23161 }));
-  ypoints.emplace_back(std::array<double, 3>({ -1.36112, 1.46405, 5.39199 }));
-  ypoints.emplace_back(std::array<double, 3>({ -1.46795, 1.43467, 11.004 }));
-  auto ycostfunc = Yerror::Create(ypoints);
-  problem.AddResidualBlock(ycostfunc, nullptr, r_vec);
+  std::vector<cv::Point3d> points;
+  
+  std::vector<std::vector<int>> places[3];
+  std::string points_file("D:\\Projects\\Documents\\20200507/points.yaml");
+  {
+    cv::FileStorage config(points_file, cv::FileStorage::READ);
+    CHECK(config.isOpened());
+    cv::read(config["points3d"], points);
+    config["x"] >> places[0];
+    config["y"] >> places[1];
+    config["z"] >> places[2];
+    config.release();
+    LOG(ERROR) << "x = " << places[0].size();
+    LOG(ERROR) << "y = " << places[1].size();
+    LOG(ERROR) << "z = " << places[2].size();
+    LOG(ERROR) << "points size = " << points.size();
+  }
 
-  xpoints.emplace_back(std::array<double, 3>({ 1.56694, 1.23576, 8.09722 }));
-  xpoints.emplace_back(std::array<double, 3>({ 1.61549, 1.17857, 4.253 }));
-  auto xcostfunc = Xerror::Create(xpoints);
-  problem.AddResidualBlock(xcostfunc, nullptr, r_vec);
+  for (int i = 0; i < 3; i++) {
+    add_problems(problem, r_vec, points, places, i);
+  }
 
-  xpoints.clear();
-  xpoints.emplace_back(std::array<double, 3>({ 1.83658, 1.16982, 4.30788 }));
-  xpoints.emplace_back(std::array<double, 3>({ 1.63993, 1.12446, 7.39286 }));
-  xcostfunc = Xerror::Create(xpoints);
-  problem.AddResidualBlock(xcostfunc, nullptr, r_vec);
-
-  xpoints.clear();
-  xpoints.emplace_back(std::array<double, 3>({ -1.6854, 1.51227, 11.4332 }));
-  xpoints.emplace_back(std::array<double, 3>({ -1.47764, 1.41993, 5.23161 }));
-  xcostfunc = Xerror::Create(xpoints);
-  problem.AddResidualBlock(xcostfunc, nullptr, r_vec);
-
-  xpoints.clear();
-  xpoints.emplace_back(std::array<double, 3>({ -1.36112, 1.46405, 5.39199 }));
-  xpoints.emplace_back(std::array<double, 3>({ -1.46795, 1.43467, 11.004 }));
-  xcostfunc = Xerror::Create(xpoints);
-  problem.AddResidualBlock(xcostfunc, nullptr, r_vec);
-
-  zpoints.emplace_back(std::array<double, 3>({ 1.56694, 1.23576, 8.09722 }));
-  zpoints.emplace_back(std::array<double, 3>({ 1.63993, 1.12446, 7.39286 }));
-  auto zcostfunc = Zerror::Create(zpoints);
-  problem.AddResidualBlock(zcostfunc, nullptr, r_vec);
-
-  zpoints.clear();
-  zpoints.emplace_back(std::array<double, 3>({ 1.61549, 1.17857, 4.253 }));
-  zpoints.emplace_back(std::array<double, 3>({ 1.83658, 1.16982, 4.30788 }));
-  zcostfunc = Zerror::Create(zpoints);
-  problem.AddResidualBlock(zcostfunc, nullptr, r_vec);
-
-  zpoints.clear();
-  zpoints.emplace_back(std::array<double, 3>({ -1.6854, 1.51227, 11.4332 }));
-  zpoints.emplace_back(std::array<double, 3>({ -1.46795, 1.43467, 11.004 }));
-  zcostfunc = Zerror::Create(zpoints);
-  problem.AddResidualBlock(zcostfunc, nullptr, r_vec);
-
-  zpoints.clear();
-  zpoints.emplace_back(std::array<double, 3>({ -1.47764, 1.41993, 5.23161 }));
-  zpoints.emplace_back(std::array<double, 3>({ -1.36112, 1.46405, 5.39199 }));
-  zcostfunc = Zerror::Create(zpoints);
-  problem.AddResidualBlock(zcostfunc, nullptr, r_vec);
-
-#endif
   std::cout << "resudual number = " << problem.NumResidualBlocks() << "," << problem.NumResiduals() << std::endl;
 
   ceres::Solver::Options options;
@@ -207,20 +195,20 @@ int main() {
   LOG(ERROR) << "FindRotation:" << summary.BriefReport();
   LOG(ERROR) << "vector = " << r_vec[0] << "," << r_vec[1] << "," << r_vec[2];
   cv::Vec3d r(r_vec[0], r_vec[1], r_vec[2]);
-  cv::Mat R;
+  cv::Matx33d R;
   cv::Rodrigues(r, R);
   LOG(ERROR) << "Get R:\n" << R;
 
-
-  all = ypoints;
   double y_norm = 0;
-  for (int i = 0; i < all.size(); i++) {
-    cv::Matx31d p(all[i][0], all[i][1], all[i][2]);
-    cv::Mat cp = (R * p).t();
-    LOG(ERROR) << p.t() << "-" << cp;
-    y_norm += cp.at<double>(0, 1);
+  for (int i = 0; i < points.size(); i++) {
+    auto cp = (R * points[i]);
+    LOG(ERROR) << "[" << i << "]=" << points[i] << "-" << cp;
+    if (i < 6) {
+      y_norm += cp.y;
+    }
+    
   }
-  LOG(ERROR) << "y = " << y_norm / all.size();
+  LOG(ERROR) << "y = " << y_norm / 6;
   system("pause");
   return 0;
 }
